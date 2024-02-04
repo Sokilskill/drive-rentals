073de640e361c024c0b3f9558781db8d5dec6c99
Sokilskill <vprovorotov@gmail.com>
2024-02-04 11:32:36 +0200

    Update selectors.js

--- src/redux/advert/selectors.js
+++ src/redux/advert/selectors.js
@@ -1,5 +1,29 @@
-export const selectIsLoading = (state) => state.adverts.isLoading;
+import { createSelector } from 'reselect';
 
+export const selectIsLoading = (state) => state.adverts.isLoading;
 export const selectDataList = ({ adverts }) => adverts.data;
 
 export const selectError = ({ adverts }) => adverts.error;
+
+const selectPrevDataListLength = ({ adverts }) => adverts.prevDataListLength;
+export const selectGetLimit = (state) => state.adverts.limit;
+
+export const selectSelectedMake = ({ adverts }) => {
+  if (adverts.selectedMake === 'All') {
+    return '';
+  } else {
+    return adverts.selectedMake;
+  }
+};
+export const selectPageNumber = ({ adverts }) => adverts.page;
+
+export const selectShowLoadMore = createSelector(
+  [selectGetLimit, selectPrevDataListLength],
+  (limit, prevDataListLength) => {
+    if (prevDataListLength < limit) {
+      return false;
+    } else {
+      return true;
+    }
+  }
+);
