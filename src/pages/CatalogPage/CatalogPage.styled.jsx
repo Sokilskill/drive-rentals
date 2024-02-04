0cbda01e6bb2e7915d1ed319636995e93483ff3a
Sokilskill <vprovorotov@gmail.com>
2024-02-04 11:32:52 +0200

    Create CatalogPage.styled.jsx

---
+++ src/pages/CatalogPage/CatalogPage.styled.jsx
@@ -0,0 +1,13 @@
+import styled from '@emotion/styled';
+
+export const Container = styled.section`
+  padding: 50px 0 100px;
+`;
+
+export const WrapperFilter = styled.div`
+  margin-bottom: 50px;
+`;
+
+export const WrapperList = styled.div`
+  margin-bottom: 100px;
+`;
