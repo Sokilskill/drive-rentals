862e110ce677e37862e928d1ca977686a305c48c
Sokilskill <vprovorotov@gmail.com>
2024-02-04 11:33:00 +0200

    Update LoadMoreButton.jsx

--- src/components/LoadMoreButton/LoadMoreButton.jsx
+++ src/components/LoadMoreButton/LoadMoreButton.jsx
@@ -1,27 +1,9 @@
-import { useDispatch } from 'react-redux';
 import { Button } from './LoadMoreButton.styled';
-import { useState } from 'react';
-import { fetchAdverts } from '../../redux/advert/operations';
 
-export const LoadMore = () => {
-  const dispatch = useDispatch();
-  const [page, setPage] = useState(2);
-  const [showBtn, setShowBtn] = useState(true);
-
-  const handlerLoadMoreBtn = async () => {
-    const { payload } = await dispatch(fetchAdverts(page));
-
-    if (payload.length < 12) {
-      setShowBtn(false);
-    }
-    setPage(page + 1);
-  };
-
+export const LoadMore = ({ handlerBtn }) => {
   return (
-    showBtn && (
-      <Button onClick={handlerLoadMoreBtn} type="button">
-        Load more
-      </Button>
-    )
+    <Button onClick={handlerBtn} type="button">
+      Load more
+    </Button>
   );
 };
