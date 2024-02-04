c7646767cb122b5540eeae96795dffcaefc64e76
Sokilskill <vprovorotov@gmail.com>
2024-02-04 11:32:58 +0200

    Update LoadMoreButton.styled.jsx

--- src/components/LoadMoreButton/LoadMoreButton.styled.jsx
+++ src/components/LoadMoreButton/LoadMoreButton.styled.jsx
@@ -1,12 +1,13 @@
 import styled from '@emotion/styled';
 
 export const Button = styled.button`
+  display: block;
+  margin: 0 auto;
   color: rgb(52, 112, 255);
   font-size: 16px;
   font-weight: 500;
   line-height: 1.5;
   text-decoration-line: underline;
-
   &:hover,
   &:focus {
     color: rgb(11, 68, 205);
