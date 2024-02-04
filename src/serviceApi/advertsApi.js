0065878e0f64f1ce2d52c7ca0e3752717e21d7e2
Sokilskill <vprovorotov@gmail.com>
2024-02-04 11:32:05 +0200

    update

--- src/serviceApi/advertsApi.js
+++ src/serviceApi/advertsApi.js
@@ -2,26 +2,12 @@ import axios from 'axios';
 
 axios.defaults.baseURL = 'https://65ba47b6b4d53c0665527927.mockapi.io/api';
 
-export const getAdverts = async (page = 1) => {
-  console.log('page in axios ', page);
+export const getAdverts = async (params) => {
+  console.log('page in axios ', params);
   const res = await axios.get('/adverts', {
-    params: {
-      limit: 12,
-      page: page,
-    },
+    params,
   });
 
   console.log('res', res);
   return res.data;
-};
-
-export const getFavoritesAdverts = async (page = 1) => {
-  const { data } = await axios.get('/favorites', {
-    params: {
-      limit: 12,
-      page: page,
-    },
-  });
-
-  return data;
 };
