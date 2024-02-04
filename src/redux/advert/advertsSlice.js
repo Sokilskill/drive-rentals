148786681375dac176accb1a55ca78dece215284
Sokilskill <vprovorotov@gmail.com>
2024-02-04 11:32:49 +0200

    Update advertsSlice.js

--- src/redux/advert/advertsSlice.js
+++ src/redux/advert/advertsSlice.js
@@ -3,14 +3,29 @@ import { fetchAdverts } from './operations';
 
 const initialState = {
   data: [],
-  favorites: [],
   isLoading: false,
   error: null,
+  limit: 12,
+  prevDataListLength: 12,
+  page: 1,
+  selectedMake: '',
 };
 
 const advertSlice = createSlice({
   name: 'adverts',
   initialState,
+  reducers: {
+    incrementPage(state) {
+      state.page = state.page + 1;
+    },
+
+    setSelectedMake(state, action) {
+      state.selectedMake = action.payload;
+    },
+    clearDataList(state) {
+      (state.data = []), (state.prevDataListLength = 12), (state.page = 1);
+    },
+  },
   extraReducers: (builder) =>
     builder
       .addCase(fetchAdverts.pending, (state) => {
@@ -19,6 +34,7 @@ const advertSlice = createSlice({
       })
       .addCase(fetchAdverts.fulfilled, (state, action) => {
         state.data = [...state.data, ...action.payload];
+        state.prevDataListLength = action.payload.length;
         state.isLoading = false;
       })
       .addCase(fetchAdverts.rejected, (state, { payload }) => {
@@ -26,6 +42,9 @@ const advertSlice = createSlice({
         state.isLoading = false;
       }),
 });
+
+export const { clearDataList, setSelectedMake, incrementPage } =
+  advertSlice.actions;
 
 const advertReducer = advertSlice.reducer;
 export default advertReducer;
